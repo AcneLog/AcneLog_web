319b7b7c7cb85968cf03de4dc65f61f9b04e148b
sseuldev <lhs010107@gmail.com>
2025-05-15 16:41:14 +0900

    [#3] feat: footer 구현

---
+++ src/components/Footer.styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.footer`
+  position: fixed;
+  bottom: 0;
+  left: 50%;
+  width: 100%;
+  background-color: ${({ theme }) => theme.mainGrey};
+  text-align: center;
+  padding: 0.8rem;
+  box-shadow: 0 -1px 5px rgba(0, 0, 0, 0.1);
+  transform: translateX(-50%);
+  max-width: 600px;
+  display: flex;
+`;
+
+export const ContainerBox = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.3rem;
+`;
+
+export const PointerBox = styled.div`
+  cursor: pointer;
+  img {
+    cursor: pointer;
+  }
+`;
