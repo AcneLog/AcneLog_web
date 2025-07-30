59d3e0cba841c545c4f8812a70eabdb88f87bddb
sungahChooo <170587664+sungahChooo@users.noreply.github.com>
2025-07-30 22:26:05 +0900

    [#22] 아크네 진단 결과 페이지 라우팅 수정

--- src/pages/diagnosis/Result.tsx
+++ src/pages/diagnosis/Result.tsx
@@ -1,24 +1,19 @@
-import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import * as S from './Result.styles';
-import { resultMap } from './resultDummyData';
-import { useState } from 'react';
 import BasicModal from '../../components/common/BasicModal';
+import { Result as ResultType } from './resultDummyData';
+import { useState } from 'react';
 
 const Result = () => {
-  const { id } = useParams(); // 주소창에서 /result/:id 를 가져옴
-  const result = resultMap[id!]; // id에 해당하는 결과 불러오기
+  const location = useLocation();
+  const result = location.state?.result as ResultType | undefined;
 
-  // 모달 적용
   const [modalOpen, setModalOpen] = useState(false);
-  const [modalConfig, setModalConfig] = useState<{
-    message: string;
-    confirmLink: string;
-  }>({
+  const [modalConfig, setModalConfig] = useState({
     message: '',
     confirmLink: '/',
   });
 
-  // 저장 버튼 모달
   const openSaveModal = () => {
     setModalConfig({
       message: '진단 결과를 저장하시겠습니까?',
@@ -27,7 +22,6 @@ const Result = () => {
     setModalOpen(true);
   };
 
-  // 홈 버튼 모달
   const openHomeModal = () => {
     setModalConfig({
       message: '진단 결과가 저장되지 않습니다. \n그래도 나가시겠습니까?',
@@ -40,14 +34,12 @@ const Result = () => {
 
   return (
     <S.Content>
-      <S.Title style={{ margin: '0 auto 2rem', width: 'fit-content' }}>진단 결과 확인하기</S.Title>
+      <S.Title>진단 결과 확인하기</S.Title>
 
-      <S.Image src={result.imageUrl} alt="사용자 업로드 이미지" />
-      <hr style={{ border: 'none', borderTop: '0.1rem dotted #ccc', margin: '2rem 0' }} />
+      <S.Image src={result.imageUrl} alt="업로드 이미지" />
 
       <S.InfoSection>
         <S.BlackBadge>진단결과</S.BlackBadge>
-        이미지 분석 결과,{'    '}
         <S.YellowBadge style={{ color: 'red' }}>{result.diagnosisName}</S.YellowBadge>일 확률이 가장
         높습니다.
         <S.Description>
@@ -97,23 +89,18 @@ const Result = () => {
           </div>
         ))}
       </S.RecommendSection>
+
       <S.ButtonSection>
         <S.BlueButton onClick={openSaveModal}>저장하기</S.BlueButton>
         <S.BlackButton onClick={openHomeModal}>홈으로 돌아가기</S.BlackButton>
       </S.ButtonSection>
 
-      {/* 모달 컴포넌트 */}
       <BasicModal
         open={modalOpen}
         onClose={() => setModalOpen(false)}
         message={modalConfig.message}
         confirmLink={modalConfig.confirmLink}
       />
-
-      <br />
-      <br />
-      <br />
-      <br />
     </S.Content>
   );
 };
