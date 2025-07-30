c31ee34ee6a58b41607f86dc71e84eaf66d8a4b9
sseuldev <lhs010107@gmail.com>
2025-07-31 02:30:09 +0900

    [#24] feat: 나의 피부 타입 진단하기 UI 구현

--- src/pages/SkinDiagnosis/SkinDiagnosis.styles.ts
+++ src/pages/SkinDiagnosis/SkinDiagnosis.styles.ts
@@ -54,6 +54,7 @@ export const BtnLayout = styled.div`
   justify-content: center;
   align-items: center;
   padding-top: 1rem;
+  margin-bottom: 4rem;
 `;
 
 // 버튼
@@ -79,4 +80,98 @@ export const SkinTypeColor = styled.span<{ skinType: string }>`
     rgba(255, 255, 0, 0.5) 100%
   );
   display: inline;
+`;
+
+// [피부 타입 진단하기] 전체 레이아웃
+export const SkinLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  padding: 0.5rem 2rem;
+  justify-content: center;
+  align-items: center;
+`;
+
+// [피부 타입 진단하기] 타이틀 레이아웃
+export const TitleLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+// x 아이콘
+export const X_Icon = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  img {
+    width: 10%;
+    height: 1.6rem;
+    cursor: pointer;
+  }
+`;
+
+// [피부 타입 진단하기] 문항 박스 모아둔 전체 레이아웃
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+// [피부 타입 진단하기] 문항 박스
+export const QuestionCard = styled.div`
+  border: 0.5px dashed ${({ theme }) => theme.subGrey};
+  border-radius: 15px;
+  padding: 2rem;
+`;
+
+// [피부 타입 진단하기] 문항 제목
+export const QuestionText = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  font-size: 1.1rem;
+  font-weight: bold;
+  line-height: 1.3;
+`;
+
+// [피부 타입 진단하기] radio 레이아웃
+export const RadioGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.7rem;
+  padding: 1rem 1rem 0 1rem;
+  font-weight: normal;
+`;
+
+// [피부 타입 진단하기] radio 텍스트
+export const Label = styled.label`
+  display: flex;
+  align-items: center;
+  gap: 0.7rem;
+  font-size: 0.95rem;
+
+  input[type='radio'] {
+    accent-color: ${({ theme }) => theme.roundBlue};
+    width: 1.1rem;
+    height: 1.1rem;
+  }
+
+  // 라디오 버튼 선택 시 스타일링
+  input[type='radio']:checked + span {
+    color: ${({ theme }) => theme.roundBlue};
+    font-weight: bold;
+  }
+`;
+
+// [피부 타입 진단하기] 제출하기 버튼
+export const SubmitButton = styled.button`
+  width: 50%;
+  padding: 1rem;
+  background-color: black;
+  color: white;
+  border-radius: 12px;
+  font-size: ${({ theme }) => theme.fontSize.xxs};
+  cursor: pointer;
+  margin-top: 1.5rem;
+  margin-bottom: 10rem;
 `;
