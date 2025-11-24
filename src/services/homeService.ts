538ccb2d92ccee4e32f49153f2c6fdd737c4c195
sungahChooo <170587664+sungahChooo@users.noreply.github.com>
2025-11-24 23:56:02 +0900

    [#46] feat: 홈 페이지 오늘의 유튜브, 오늘의 추천템 API 연동 및 스타일 수정

--- src/services/homeService.ts
+++ src/services/homeService.ts
@@ -4,10 +4,34 @@ export type AcnePostCountData = Record<
   'COMEDONES' | 'PUSTULES' | 'PAPULES' | 'FOLLICULITIS',
   number
 >;
-
+export interface RecommendViedoItem {
+  videoId: number;
+  videoUrl: string;
+  videoTitle: string;
+  thumbnailUrl: string;
+}
+export interface RecommendProductItem {
+  productId: number;
+  productUrl: string;
+  productName: string;
+  productImage: string;
+}
 export const homePeoplesLogService = {
   async getPeoplesLog(): Promise<AcnePostCountData> {
     const res = await apiPublic.get('/analysis/main');
+    // console.log(res.data.data);
+    return res.data.data;
+  },
+};
+
+export const recommendService = {
+  async getRecommendVideo(): Promise<RecommendViedoItem[]> {
+    const res = await apiPublic.get('/analysis/main/youtube');
+    // console.log(res.data.data);
+    return res.data.data;
+  },
+  async getRecommendProduct(): Promise<RecommendProductItem[]> {
+    const res = await apiPublic.get('analysis/main/product');
     // console.log(res.data.data);
     return res.data.data;
   },
